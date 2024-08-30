ef7f2a5613a36a9f749c90b5f3b57e418ab74388
Rohitharish <rohitharish276@gmail.com>
2024-08-31 01:16:37 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,21 @@ import ScrollProvider from "./components/Scrollprovider";
 import Intro from "./components/Intro";
 import ImageAnimator from "./components/Experience/Slide";
 import Navbar from "./components/Navbar";
-import Scene from "./components/3delement/Scene";
+
 import Preloader from "./components/Preloader";
+import dynamic from "next/dynamic";
+
+const DynamicScene = dynamic(() => import("./components/3delement/Scene"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <ScrollProvider>
       <Preloader />
       <Navbar />
+      <DynamicScene />
 
-      <Scene />
       <main className="flex flex-col h-full w-full  items-center  bg-black ">
         <Intro />
         <Project />
