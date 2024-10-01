b883ea891f5859d359513f47d5d21363ffa0cf8e
Rohitharish <rohitharish276@gmail.com>
2024-10-02 01:02:42 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,6 @@ import ScrollProvider from "./components/Scrollprovider";
 
 import Preloader from "./components/Preloader";
 import dynamic from "next/dynamic";
-import Intro from "./components/Intro";
-import Project from "./pages/Project/page";
-import Experience from "./components/Experience/Moshi";
-import Menu from "./components/Menu/Menu";
 
 const DynamicScene = dynamic(() => import("./components/3delement/Scene"), {
   ssr: false,
